5832cd60576d5d9d6f8707a564395f99bd0e1778
Paul <paul.dibenedetto@concept-solutions.com>
2021-06-08 14:39:09 -0400

    Updating for small Web MVC application

--- src/main/java/com/appsdeveloperblog/ws/api/albums/AlbumsResourceServerApplication.java
+++ src/main/java/com/appsdeveloperblog/ws/api/albums/AlbumsResourceServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class AlbumsApplication {
+public class AlbumsResourceServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AlbumsApplication.class, args);
+        SpringApplication.run(AlbumsResourceServerApplication.class, args);
     }
 
 }
